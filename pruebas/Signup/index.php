0c10ea3119d73ccd740d175c73f4c2ce86bf890f
sgilcastro <susanagilcastro@gmail.com>
2023-02-03 23:41:08 +0100

    pequeñas correcciones

--- pruebas/Signup/index.php
+++ pruebas/Signup/index.php
@@ -8,7 +8,7 @@
       Nombre:<input name="name" type="text"><br>
       Apellido:<input name="surname" type="text"><br>
       Nombre Usuario:<input name="userName" type="text"><br>
-      Email:<input name="email" type="text"><br>
+      Email:<input name="email" type="email"><br>
       Password:<input name="pw" type="text"><br>
       <input type="submit" value="Enviar">
     </form>
