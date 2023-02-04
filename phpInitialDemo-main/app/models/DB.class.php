871e5f656150ebc4b1ed462ce0417d9c1505591a
Rodri Saiz <rodrisaiz@icloud.com>
2023-02-04 14:17:31 +0100

    DB correction and class optimitation

--- phpInitialDemo-main/app/models/DB.class.php
+++ phpInitialDemo-main/app/models/DB.class.php
@@ -1,36 +1,21 @@
 <?php
 
 
-class DB{
+    class DB{
 
-    public $decoded_json;
-    public $users;
-    public $tasks;
-    public $datarequest;
+        public $decoded_json;
 
 
-    public function decode(){
+            public function read(){
 
-        $this->decoded_json = json_decode(file_get_contents('DB.json'), true);
+                $this->decoded_json = json_decode(file_get_contents('DB.json'), true);
 
-        return $this->decoded_json;
+                return $this->decoded_json;
 
 
-    }
-    
-
-    public function read(){
-
-        $this->users = $this->decode()['users'];
-
-       return $this->users;
+            }
 
     }
-
-}
-
-
-
 
 ?>
 
