3187be87d2c9588d42e29d0cb588557946ae20b1
Rodri Saiz <rodrisaiz@icloud.com>
2023-02-04 10:24:29 +0100

    login class

--- phpInitialDemo-main/app/models/DB.class.php
+++ phpInitialDemo-main/app/models/DB.class.php
@@ -7,6 +7,9 @@ class DB{
     public $users;
     public $tasks;
     public $datarequest;
+    public $username = "sgil";
+    public $pwd = "1234";
+    public $variables;
 
 
     public function decode(){
@@ -27,10 +30,169 @@ class DB{
 
     }
 
+
+
+/*
+    public function __construct($decoded_json, $users, $tasks , $datarequest){
+
+        $this->decoded_json = $decoded_json;
+        $this->users = $users;
+        $this->tasks = $tasks;
+        $this->datarequest = $datarequest;
+
+
+
+    }
+
+
+
+    public function decode(){
+
+        $this->decoded_json = json_decode(file_get_contents('DB.json'), true);
+
+        echo $decoded_json;
+
+    }
+
+
+    public function users(){
+
+        return $this->users = $decoded_json['users'];
+
+       
+
+    }
+
+/*
+    public function tasks(){
+
+        return $this->tasks = $decoded_json['tareas'];
+
+    }
+
+  */
+
+    
+
+}
+
+
+
+    
+
+?>
+
+<br>
+<br>
+<br>
+
+<?php
+/*
+
+$objet1 = new DB();
+
+//echo $objet1->loging();
+
+
+
+
+print_r('<br>');
+print_r('<br>');
+print_r('<br>');
+
+
+print_r( $objet1->read());
+
+
+print_r('<br>');
+print_r('<br>');
+print_r('<br>');
+
+
+
+
+//print_r( $objet1->tasks());
+
+
+
+/*
+
+
+
+
+class DB{
+
+    private $decoded_json;
+    public $users;
+    public $tasks;
+    public $datarequest = "name";
+
+
+        
+
+    public function __construct($users, $tasks, $datarequest){
+
+        $this->users = $users;
+        $this->tasks = $tasks;
+        $this-> $datarequest = "name";
+
+    }   
+
+
+
+    private function decode(){
+
+        $this->decoded_json = json_decode(file_get_contents('DB.json'), true);
+
+    }
+
+    private function users(){
+
+        return $this->users = $decoded_json['users'];
+
+       
+
+    }
+
+
+    private function tasks(){
+
+        return $this->tasks = $decoded_json['tareas'];
+
+    }
+
+
+
+    public function readData()
+    {
+
+        foreach($this->users as $user){
+
+             $data = $user[$this->datarequest]. '.<br>'; 
+
+            //return $data;
+            //$surname = $user['surname'];
+            //$tareas = $user['tareas'];
+            
+            
+            return $data;
+
+    }
+
+    }
+
+
+
 }
 
 
 
 
-?>
+$db = new DB();
 
+echo $db->readData();
+
+
+//echo readData($datarequest);
+*/
+?>
--- phpInitialDemo-main/app/models/login.class.php
+++ phpInitialDemo-main/app/models/login.class.php
@@ -1,17 +1,129 @@
 <?php
 
 
+
 class Login extends DB{
+ 
 
-    public function loging(){
 
-        
+    //public $username = $_POST["user"]
+    public $username = "usuario";
+    public $pw = "1234";
+    public $variables; 
+    public $variables2; 
+    public $user; 
+    public $pasword;
+    public $pasword1;  
+    public $result; 
+    public $contador = -1; 
+
+
+
+
+
+    public function userComprobation(){
+
+        foreach($this->read() as $variables ){  
+
+            $this->user = $variables['user']; 
+
+            $this->contador = $this->contador + 1;
+            
+            if ($this->username == $this->user){
+
+                    $this->result = "Lo has logrado!!!!!!" . $this->contador;
+
+                }else{
+
+                    $this->result = "NO lo has logrado";
+
+                }        
+        }
+
+        return $this->result;
     }
 
+
+
+
+
+public function pwComprobation(){
+
+    //foreach($this->read() as $variables ){  
+
+        $this->pasword1 = $this->read()[$this->contador ]; 
+
+                
+            $this->pasword = $this->pasword1['pw']; 
+        
+        if ($this->pasword == $this->pwd){
+        //hasta aca funciona 
+            $this->result = "Lo has logrado!!!!!!";
+
+            }else{
+
+            $this->result = "NO lo has logrado";
+
+            }
+
+            return $this->result;
+
     
+
+}
 }
 
 
 
+     /*   foreach($this->read() as $variables2 ){
+
+            $this->pasword = $variables2['user'][$this->user];
+
+
+            if ($this->pasword == $this->$pwd ){
+
+                echo "Lo has logrado!!!!!!";
+
+
+            }else{
+
+            
+
+            echo "NO lo has logrado";
+
+        }
+    }
+
+/*
+
+        foreach($users as $user) {
+            $name = $user['name']; 
+            $surname = $user['surname'];
+            $tareas = $user['tareas'];
+            foreach($tareas as $tarea) {
+                echo $name.' tienes que '. $tarea['titulo'].' esta tarea está '.$tarea['estado'].'.<br>';
+            }
+        }
+
+        return $this->variable;
+    }
+*/
+
+
+
+
+?>
+
+<?php
+
+
+
+$objet1 = new Login();
+
+echo $objet1->userComprobation();
+echo $objet1->pwComprobation();
+
+
+//print_r($objet1->$this->read());
 
 ?>
