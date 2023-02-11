f4f535a0c27a74b1455a92f230fdf316f30bc1d6
Rodri Saiz <rodrisaiz@icloud.com>
2023-02-11 13:47:23 +0100

    Modify routes

--- phpInitialDemo-main/config/routes.php
+++ phpInitialDemo-main/config/routes.php
@@ -10,17 +10,28 @@
  * '/calendar' => 'calendar#index'
  */
 $routes = array(
+
 	'/' => 'login#index',
 	'/loginerror' => 'login#error',
 	'/login' => 'login#login',
 	'/signuphome' => 'signup#index',
 	'/signup' => 'signup#signup',
 	'/home' => 'home#home',
-	'/task' => 'general#task',
-	'/taskup' => 'taskup#taskup',
-	'/taskuphome' => 'taskup#index',
+	'/task' => 'task#taskup',
+	'/taskhome' => 'task#index',
+	'/taskedithome' => 'task#edithome',
+	'/taskedit' => 'task#edit',
+	'/user' => 'user#index',
+	'/modifyuser' => 'user#userModify',
+	'/deleteuser' => 'user#delete',
+	'/deleteuser' => 'user#delete',
+	
+
 // the following are just test
 	'/test' => 'test#index',
 	'/testa' => 'test#check',
 	'/rodri' => 'rodri#index'
+
 );
+
+?>
